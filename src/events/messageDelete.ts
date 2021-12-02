bcf1a0a5f55d3155b4a08b0382eb2047b3d01593
pluffie <igor.pluffie@gmail.com>
2021-12-02 16:24:54 +0300

    Add auto-actions api

--- src/events/messageDelete.ts
+++ src/events/messageDelete.ts
@@ -1,27 +1,47 @@
-import { Client, Message, MessageEmbed } from "discord.js"
+import { Client, Message, MessageActionRow, MessageButton, MessageEmbed, MessageOptions } from "discord.js"
 
 const defaultReason = "No reason provided"
 var reason: string = defaultReason
+var isAutoAction: boolean = false
 
 export function setReason(newReason: string) {
     reason = newReason
+}
+
+export function setAutoAction() {
+    isAutoAction = true
 }
 
 export function execute(client: Client) {
     return (msg: Message) => {
         if (msg.content == "") return
 
-        msg.guild.systemChannel.send({
+        const logMsgData: MessageOptions = {
             embeds: [
                 new MessageEmbed()
                     .setColor("#d50000")
                     .setTitle("Message was deleted")
                     .addField("Content", `> ${msg.content}`, false)
                     .addField("Author", `> ${msg.author}`, true)
+                    .addField("Channel", `> ${msg.channel}`, true)
                     .addField("Reason", `> ${reason}`, true)
                     .setTimestamp()
             ]
-        })
+        }
+
+        if (isAutoAction)
+            logMsgData.components = [
+                new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel("Ban")
+                            .setStyle("DANGER")
+                    )
+            ]
+
+        msg.guild.systemChannel.send(logMsgData)
+
         reason = defaultReason
+        isAutoAction = false
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { GuildTextBasedChannel, Message } from "discord.js"
-import { setReason } from "./events/messageDelete"
+import { setAutoAction, setReason } from "./events/messageDelete"
 import * as logger from "./logger"
 
 /**
@@ -27,12 +27,14 @@ export function messageFilter(msg: Message): boolean {
     const lastMsg = msg.channel.messages.cache.filter(m => m.author == msg.author).last(2)
     const timeElapsed = msg.createdTimestamp - lastMsg[0]?.createdTimestamp
     if (timeElapsed < 500 && timeElapsed > 0) {
+        setAutoAction()
         setReason("Flood")
         msg.delete()
         return false
     }
 
     if (lastMsg[1]?.content == lastMsg[0]?.content) {
+        setAutoAction()
         setReason("Flood")
         lastMsg[1].delete()
         return false
