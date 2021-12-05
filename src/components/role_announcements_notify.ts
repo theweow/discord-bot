61988f7de73ee89cc1db04f36a726daa023538dd
pluffie <59744842+pluffie@users.noreply.github.com>
2021-12-06 00:39:21 +0300

    FIx fake "Interaction failed" message

--- src/components/role_announcements_notify.ts
+++ src/components/role_announcements_notify.ts
@@ -3,4 +3,5 @@ import { roles } from "../config.json"
 
 export function execute(interaction: ButtonInteraction) {
     (interaction.member.roles as GuildMemberRoleManager).add(roles.announcements_notify)
+    interaction.update({})
 }
--- src/components/role_news_notify.ts
+++ src/components/role_news_notify.ts
@@ -3,4 +3,5 @@ import { roles } from "../config.json"
 
 export function execute(interaction: ButtonInteraction) {
     (interaction.member.roles as GuildMemberRoleManager).add(roles.news_notify)
+    interaction.update({})
 }
