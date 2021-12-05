ff329928e84fdfbe8d951b31c39d10d4ffa995f5
pluffie <59744842+pluffie@users.noreply.github.com>
2021-12-06 00:26:02 +0300

    Add role selection menu

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "@discordjs/builders"
-import { ApplicationCommandPermissionData, CommandInteraction } from "discord.js"
+import { ChannelType } from "discord-api-types/v9"
+import { ApplicationCommandPermissionData, CommandInteraction, GuildTextBasedChannel, MessageActionRow, MessageButton } from "discord.js"
 
 export const data = new SlashCommandBuilder()
     .setDefaultPermission(false)
@@ -7,8 +8,15 @@ export const data = new SlashCommandBuilder()
     .setDescription("Root command for dev-only commands")
     .addSubcommand(sub =>
         sub.setName("reload")
-            .setDescription("Reloads events")
-    )
+            .setDescription("Reloads events"))
+    .addSubcommand(sub =>
+        sub.setName("rolegiver")
+            .setDescription("Generates rolegiver")
+            .addChannelOption(option =>
+                option.setName("channel")
+                    .setDescription("Channel")
+                    .setRequired(true)
+                    .addChannelType(ChannelType.GuildText)))
 
 export const permissions: ApplicationCommandPermissionData[] = [
     {
@@ -19,7 +27,28 @@ export const permissions: ApplicationCommandPermissionData[] = [
 ]
 
 export async function execute(interaction: CommandInteraction) {
-    if (interaction.options.getSubcommand() == "reload-events")
-        process.exit(0)
+    switch (interaction.options.getSubcommand()) {
+        case "reload":
+            process.exit(0)
+
+        case "rolegiver":
+            (interaction.options.getChannel("channel") as GuildTextBasedChannel).send({
+                content: "**Select role:**",
+                components: [
+                    new MessageActionRow()
+                        .addComponents(
+                            new MessageButton()
+                                .setStyle("SECONDARY")
+                                .setCustomId("role_announcements_notify")
+                                .setLabel("Announcements notification"))
+                        .addComponents(
+                            new MessageButton()
+                                .setStyle("SECONDARY")
+                                .setCustomId("role_news_notify")
+                                .setLabel("News notification"))
+                ]
+            })
+            break
+    }
     interaction.editReply("Successfully")
 }
---
+++ src/components/role_announcements_notify.ts
@@ -0,0 +1,6 @@
+import { ButtonInteraction, GuildMemberRoleManager, MessageEmbed } from "discord.js"
+import { roles } from "../config.json"
+
+export function execute(interaction: ButtonInteraction) {
+    (interaction.member.roles as GuildMemberRoleManager).add(roles.announcements_notify)
+}
